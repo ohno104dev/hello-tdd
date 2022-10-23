0ec995415d11a3046a88794569f7f77ec12ad98f
ohno104 <ohno104dev@gmail.com>
2022-10-23 23:47:41 +0800

    歐元進場

--- go/money_test.go
+++ go/money_test.go
@@ -23,3 +23,18 @@ func (d Dollar) Times(multiplier int) Dollar {
 		amount: d.amount * multiplier,
 	}
 }
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{
+		amount:   10,
+		currency: "EUR",
+	}
+	twentyEuros := tenEuros.Times(2)
+	if twentyEuros.amount != 20 {
+		t.Errorf("Expected 20, got: [%d]", twentyEuros.amount)
+	}
+
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("Expected EUR, got: [%s]", twentyEuros.currency)
+	}
+}
